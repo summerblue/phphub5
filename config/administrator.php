818d1a46eebac472942c5b9bb7582e2090bcb27b
zhengjinghua <jin114001251@gmail.com>
2016-07-11 23:29:15 +0800

    Update administrator config

--- config/administrator.php
+++ config/administrator.php
@@ -134,7 +134,7 @@ return array(
      *
      * @type string
      */
-    'login_path' => 'auth/login',
+    'login_path' => '/login',
 
     /*
      * The logout path is the path where Administrator will send the user when they click the logout link
