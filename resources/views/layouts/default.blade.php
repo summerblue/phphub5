ddc505d8e608afaeed914e95c9137e8eff832adf
zhengjinghua <jin114001251@gmail.com>
2016-07-12 17:25:32 +0800

    Delete useless code

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -45,8 +45,7 @@ PHPHub  - PHP & Laravel的中文社区
                     'notificationsCount' : '{{ route('notifications.count') }}',
                     'upload_image' : '{{ route('upload_image') }}'
                 },
-                'token': '{{ csrf_token() }}',
-				'is_admin': '{{is_admin() ? 'yes' : 'no'}}'
+                'token': '{{ csrf_token() }}'
             };
 
 			var ShowCrxHint = '{{isset($show_crx_hint) ? $show_crx_hint : 'no'}}';
