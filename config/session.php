ab1e1dcb32b324e9a08a98e5ec4f02b6c6361570
Charlie Jade <cj@estgroupe.com>
2016-07-12 08:04:14 +0800

    change session expire time

--- config/session.php
+++ config/session.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'lifetime' => 120,
+    'lifetime' => 525600,
 
     'expire_on_close' => false,
 
@@ -135,7 +135,7 @@ return [
     |
     */
 
-    'domain' => null,
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
