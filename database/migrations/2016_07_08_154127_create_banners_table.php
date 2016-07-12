d7bef98baa432a179030530b6cdd4d9636b77dfe
zhengjinghua <jin114001251@gmail.com>
2016-07-12 14:48:26 +0800

    Add 'target' for banner

--- database/migrations/2016_07_08_154127_create_banners_table.php
+++ database/migrations/2016_07_08_154127_create_banners_table.php
@@ -3,35 +3,36 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBannersTable extends Migration {
+class CreateBannersTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('banners', function(Blueprint $table) {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
             $table->increments('id');
             $table->string('position')->index();
             $table->integer('order')->unsigned()->default(0)->index();
             $table->string('image_url');
             $table->string('title')->index();
             $table->string('link')->nullable();
+            $table->enum('target', ['_blank',  '_self'])->default('_blank')->index();
             $table->text('description')->nullable();
             $table->timestamps();
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('banners');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('banners');
+    }
 }
