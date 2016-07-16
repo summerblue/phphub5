7009a3dc571dac6ba88866d3f177c226925fdc3d
Summer <summer@yousails.com>
2016-07-16 15:40:58 +0800

    refactoring using trait

---
+++ app/Models/Traits/TopicFilterable.php
@@ -0,0 +1,74 @@
+<?php namespace App\Models\Traits;
+
+use Carbon\Carbon;
+
+trait TopicFilterable
+{
+    public function getTopicsWithFilter($filter, $limit = 20)
+    {
+        return $this->applyFilter($filter)
+                    ->with('user', 'category', 'lastReplyUser')
+                    ->paginate($limit);
+    }
+
+    public function getCategoryTopicsWithFilter($filter, $category_id, $limit = 20)
+    {
+        return $this->applyFilter($filter == 'default' ? 'category' : $filter)
+                    ->where('category_id', '=', $category_id)
+                    ->with('user', 'category', 'lastReplyUser')
+                    ->paginate($limit);
+    }
+
+    public function applyFilter($filter)
+    {
+        switch ($filter) {
+            case 'noreply':
+                return $this->orderBy('reply_count', 'asc')->recent();
+                break;
+            case 'vote':
+                return $this->orderBy('vote_count', 'desc')->recent();
+                break;
+            case 'excellent':
+                return $this->excellent()->recent();
+                break;
+            case 'recent':
+                return $this->recent();
+                break;
+            case 'category':
+                return $this->recentReply();
+                break;
+            default:
+                return $this->pinAndRecentReply();
+                break;
+        }
+    }
+
+    public function scopeWhose($query, $user_id)
+    {
+        return $query->where('user_id', '=', $user_id)->with('category');
+    }
+
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
+
+    public function scopePinAndRecentReply($query)
+    {
+        return $query->whereRaw("(`created_at` > '".Carbon::today()->subMonths(3)->toDateString()."' or (`order` > 0) )")
+                     ->orderBy('order', 'desc')
+                     ->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecentReply($query)
+    {
+        return $query->orderBy('order', 'desc')
+                     ->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeExcellent($query)
+    {
+        return $query->where('is_excellent', '=', 'yes');
+    }
+}
+
