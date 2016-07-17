09b7b7bf9e451af4cbe4b240a8ec229daaf5d73a
Summer <summer@yousails.com>
2016-07-17 21:52:11 +0800

    add popover

--- resources/views/topics/partials/active_users.blade.php
+++ resources/views/topics/partials/active_users.blade.php
@@ -3,12 +3,11 @@
   @foreach ($active_users as $index => $active_user)
    <li class="list-group-item" >
 
-      <a href="{{ route('users.show', [$active_user->id]) }}" title="{{{ $active_user->name }}}">
+      <a class="popover-with-html" href="{{ route('users.show', [$active_user->id]) }}" data-content="{{{ $active_user->name . ($active_user->introduction ? ' - ' . $active_user->introduction : '')}}}">
 
         <img class="media-object img-thumbnail avatar avatar-small inline-block " src="{{ $active_user->present()->gravatar }}">
 
         {{{ $active_user->name }}}
-
 
         @if($active_user->introduction)
         <span class="introduction">
