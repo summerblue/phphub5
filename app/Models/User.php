0f2c7a4c1f2d3bafcd80350e9f38d51d960803e2
zhengjinghua <jin114001251@gmail.com>
2016-07-18 22:34:08 +0800

    Update wechat to weixin

--- app/Models/User.php
+++ app/Models/User.php
@@ -93,7 +93,7 @@ class User extends Model implements AuthenticatableContract,
     {
         $functionMap = [
             'github' => 'getByGithubId',
-            'wechat' => 'getByWechatId'
+            'weixin' => 'getByWechatId'
         ];
         $function = $functionMap[$driver];
         if (!$function) {
