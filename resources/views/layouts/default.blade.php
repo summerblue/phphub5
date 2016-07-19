405d55f83f4dc1be6e8c54673c2d161504d12aaa
Summer <summer@yousails.com>
2016-07-19 23:18:55 +0800

    mark

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -63,6 +63,13 @@ PHPHub  - PHP & Laravel的中文社区
 
 			<div class="container">
 
+				@if(\Auth::check() && \Auth::user()->verified !== true && !Request::is('email-verification-required'))
+				<div class="alert alert-warning">
+		            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+		            邮箱未激活，请前往 {{ \Auth::user()->email }} 查收激活邮件，激活后才能完整地使用社区功能，如发帖和回帖。未收到邮件？请前往 <a href="{{ route('email-verification-required') }}">重发邮件</a> 。
+		        </div>
+				@endif
+
 				@include('flash::message')
 
 				@yield('content')
