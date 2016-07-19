52f0ca8944d3b7df660b036f8f6c572b5cd988af
zhengjinghua <jin114001251@gmail.com>
2016-07-19 16:19:29 +0800

    Add update email logic

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class UpdateUserRequest extends Request
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+
+        return [
+            'github_id'       => 'unique:users',
+            'github_name'     => 'string',
+            'wechat_openid'   => 'string',
+            'email'           => 'email|required|unique:users,email,' . \Auth::user()->id,
+            'github_url'      => 'active_url',
+            'image_url'       => 'active_url',
+            'wechat_unionid'  => 'string',
+        ];
+    }
+}
