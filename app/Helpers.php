2b72dc0d028af6291587856d6edef364e79e6364
zhengjinghua <jin114001251@gmail.com>
2016-07-19 09:39:50 +0800

    Refactory upload topic image logic

--- app/Helpers.php
+++ app/Helpers.php
@@ -46,3 +46,33 @@ function adminEnumStyleOutput($value)
     $class = ($value === true || $value == 'yes') ? 'success' : 'danger';
     return '<span class="label bg-'.$class.'">'.$value.'</span>';
 }
+
+function uploadTopicImage($file)
+{
+    $allowed_extensions = ["png", "jpg", "gif"];
+    if ($file->getClientOriginalExtension() && !in_array($file->getClientOriginalExtension(), $allowed_extensions)) {
+        return ['error' => 'You may only upload png, jpg or gif.'];
+    }
+
+    $fileName        = $file->getClientOriginalName();
+    $extension       = $file->getClientOriginalExtension() ?: 'png';
+    $folderName      = 'uploads/images/' . date("Ym", time()) .'/'.date("d", time()) .'/'. Auth::user()->id;
+    $destinationPath = public_path() . '/' . $folderName;
+    $safeName        = str_random(10).'.'.$extension;
+    $file->move($destinationPath, $safeName);
+
+    // If is not gif file, we will try to reduse the file size
+    if ($file->getClientOriginalExtension() != 'gif') {
+        // open an image file
+        $img = Image::make($destinationPath . '/' . $safeName);
+        // prevent possible upsizing
+        $img->resize(1440, null, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+        // finally we save the image as a new file
+        $img->save();
+    }
+
+    return ['error' => '', 'filename' => getUserStaticDomain() . $folderName .'/'. $safeName];
+}
