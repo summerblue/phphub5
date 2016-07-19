6dfff74a455a99307f06ebd8cf372bf1ed5cd6f7
Summer <summer@yousails.com>
2016-07-19 16:36:28 +0800

    share with weibo user name

--- resources/views/auth/userbanned.blade.php
+++ resources/views/auth/userbanned.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
   <div class="row">
-    <div class="col-md-4 col-md-offset-4">
+    <div class="col-md-4 col-md-offset-4 floating-box">
       <div class="panel panel-default">
         <div class="panel-heading">
           <h3 class="panel-title">{{ lang('Notice') }}</h3>
