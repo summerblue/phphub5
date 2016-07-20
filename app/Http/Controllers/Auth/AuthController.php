d411ab3d90f5d2b00b04c10b799570f2a712707e
zhengjinghua <jin114001251@gmail.com>
2016-07-20 10:56:11 +0800

    Improve auth controller logic

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller implements UserCreatorListener
      */
     public function __construct(User $userModel)
     {
-        $this->middleware('guest', ['except' => ['logout', 'getVerification', 'emailVerificationRequired']]);
+        $this->middleware('guest', ['except' => ['logout', 'emailVerificationRequired']]);
         $this->middleware('auth', ['only' => ['emailVerificationRequired']]);
     }
 
