5034a7132c665daa26b6dd3bf043fcd92dd6e58e
Summer <summer@yousails.com>
2016-07-20 09:21:44 +0800

    remove badge from topic meta info

--- resources/views/topics/partials/meta.blade.php
+++ resources/views/topics/partials/meta.blade.php
@@ -3,17 +3,14 @@
   <a href="{{ route('categories.show', $topic->category->id) }}" class="remove-padding-left">
     {{{ $topic->category->name }}}
   </a>
-  ⋅ 
+  ⋅
   <a href="{{ route('users.show', $topic->user->id) }}">
     {{{ $topic->user->name }}}
   </a>
 
-  @if ($topic->user->present()->hasBadge())
-    <span class="label label-warning" style="position: relative;">{{{ $topic->user->present()->badgeName() }}}</span>
-  @endif
-  ⋅ 
+  ⋅
   {{ lang('at') }} <abbr title="{{ $topic->created_at }}" class="timeago">{{ $topic->created_at }}</abbr>
-  ⋅ 
+  ⋅
 
   @if (count($topic->lastReplyUser))
     {{ lang('Last Reply by') }}
@@ -21,7 +18,7 @@
         {{{ $topic->lastReplyUser->name }}}
       </a>
      {{ lang('at') }} <abbr title="{{ $topic->updated_at }}" class="timeago">{{ $topic->updated_at }}</abbr>
-    ⋅ 
+    ⋅
   @endif
 
   {{ $topic->view_count }} {{ lang('Reads') }}
