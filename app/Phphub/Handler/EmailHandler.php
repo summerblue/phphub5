dc403f5484b5e576ad7b497b420425f93ed66b09
zhengjinghua <jin114001251@gmail.com>
2016-07-20 10:46:36 +0800

    Add send reply notify mail

--- app/Phphub/Handler/EmailHandler.php
+++ app/Phphub/Handler/EmailHandler.php
@@ -3,6 +3,7 @@
 namespace Phphub\Handler;
 
 use App\Models\User;
+use App\Models\Reply;
 use Illuminate\Mail\Message;
 use Mail;
 use Naux\Mail\SendCloudTemplate;
@@ -23,6 +24,21 @@ class EmailHandler
                 'url'  => url('verification', $user->verification_token).'?email='.urlencode($user->email),
             ]));
             $message->to($user->email);
+        });
+    }
+
+    public static function sendReplyNotifyMail(Reply $reply)
+    {
+        Mail::send('emails.fake', [], function (Message $message) use ($reply) {
+            $message->subject(lang('Your topic have new reply'));
+
+            $message->getSwiftMessage()->setBody(new SendCloudTemplate('new_reply', [
+                'name' => "<a href='" . url(route('users.show', $reply->user_id)) . "' target='_blank'>{$reply->user->name}</a>",
+                'title' => "<a href='" . url(route('topics.show', $reply->topic_id)) . "' target='_blank'>{$reply->topic->title}</a>",
+                'content'  => $reply->body,
+            ]));
+
+            $message->to($reply->topic->user->email);
         });
     }
 }
