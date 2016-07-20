5ed4738259d3491f631e0f26718705a518cf2fab
Charlie Jade <cj@estgroupe.com>
2016-07-20 21:56:58 +0800

    don send email if is topic author replied

--- app/Phphub/Creators/ReplyCreator.php
+++ app/Phphub/Creators/ReplyCreator.php
@@ -47,7 +47,7 @@ class ReplyCreator
 
         app('Phphub\Notification\Notifier')->newReplyNotify(Auth::user(), $this->mentionParser, $topic, $reply);
 
-        if (Auth::user()->email_notify_enabled == 'yes') {
+        if (Auth::user()->email_notify_enabled == 'yes' && $reply->user_id != Auth::id()) {
             dispatch(new SendReplyNotifyMail($reply));
         }
 
