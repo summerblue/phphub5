c5132da128df773ff06b62318d950cfbe8b0a784
zhengjinghua <jin114001251@gmail.com>
2016-07-20 09:22:29 +0800

    Refactory User model

---
+++ app/Models/Traits/UserRememberTokenHelper.php
@@ -0,0 +1,19 @@
+<?php namespace App\Models\Traits;
+
+trait UserRememberTokenHelper
+{
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+}
