837db84bd4e0d534e74deb4f5bb79597e69c0884
zhengjinghua <jin114001251@gmail.com>
2016-07-21 10:19:51 +0800

    Improve ImageUploadHandler

--- app/Phphub/Handler/ImageUploadHandler.php
+++ app/Phphub/Handler/ImageUploadHandler.php
@@ -9,60 +9,64 @@ use Auth;
 
 class ImageUploadHandler
 {
+    protected $file;
+
     public function uploadAvatar($file, User $user)
     {
+        $this->file = $file;
+
         $allowed_extensions = ["png", "jpg", "gif"];
-        if ($file->getClientOriginalExtension() && !in_array($file->getClientOriginalExtension(), $allowed_extensions)) {
+        if (!$this->checkAllowedExtensions($allowed_extensions)) {
             return ['error' => 'You may only upload png, jpg or gif.'];
         }
 
-        $fileName        = $file->getClientOriginalName();
-        $extension       = $file->getClientOriginalExtension() ?: 'png';
-        $folderName      = 'uploads/avatars';
-        $destinationPath = public_path() . '/' . $folderName;
-        $avatar_name     = $user->id . '_' . time() . '.' . $extension;
+        $avatar_name = $user->id . '_' . time() . '.' . $file->getClientOriginalExtension() ?: 'png';
+        $local_image = $this->saveImageToLocal('avatar', 380, $avatar_name);
 
-        $file->move($destinationPath, $avatar_name);
-
-        // If is not gif file, we will try to reduse the file size
-        if ($file->getClientOriginalExtension() != 'gif') {
-            // open an image file
-            $img = Image::make($destinationPath . '/' . $avatar_name);
-            // prevent possible upsizing
-            $img->resize(380, 380, function ($constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            });
-            // finally we save the image as a new file
-            $img->save();
-        }
         return ['error' => '', 'filename' => $avatar_name];
     }
 
     public function uploadImage($file)
     {
-        $allowed_extensions = ["png", "jpg", "gif"];
+        $this->file = $file;
 
-        if ($file->getClientOriginalExtension() && !in_array($file->getClientOriginalExtension(), $allowed_extensions)) {
+        $allowed_extensions = ["png", "jpg", "gif"];
+        if (!$this->checkAllowedExtensions($allowed_extensions)) {
             return ['error' => 'You may only upload png, jpg or gif.'];
         }
 
-        $fileName        = $file->getClientOriginalName();
-        $extension       = $file->getClientOriginalExtension() ?: 'png';
-        $folderName      = 'uploads/images/' . date("Ym", time()) .'/'.date("d", time()) .'/'. Auth::user()->id;
-        $destinationPath = public_path() . '/' . $folderName;
-        $safeName        = str_random(10).'.'.$extension;
-        $file->move($destinationPath, $safeName);
+        $local_image = $this->saveImageToLocal('topic', 1440);
+        return ['error' => '', 'filename' => get_user_static_domain() . $local_image];
+    }
 
-        if ($file->getClientOriginalExtension() != 'gif') {
+    protected function checkAllowedExtensions($allowed_extensions)
+    {
+        if ($this->file->getClientOriginalExtension() && !in_array($this->file->getClientOriginalExtension(), $allowed_extensions)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function saveImageToLocal($type, $resize, $filename = '')
+    {
+        $folderName = ($type == 'avatar')
+            ? 'uploads/avatars'
+            : 'uploads/images/' . date("Ym", time()) .'/'.date("d", time()) .'/'. Auth::user()->id;
+
+        $destinationPath = public_path() . '/' . $folderName;
+        $extension = $this->file->getClientOriginalExtension() ?: 'png';
+        $safeName  = $filename ? :str_random(10) . '.' . $extension;
+        $this->file->move($destinationPath, $safeName);
+
+        if ($this->file->getClientOriginalExtension() != 'gif') {
             $img = Image::make($destinationPath . '/' . $safeName);
-            $img->resize(1440, null, function ($constraint) {
+            $img->resize($resize, null, function ($constraint) {
                 $constraint->aspectRatio();
                 $constraint->upsize();
             });
             $img->save();
         }
-
-        return ['error' => '', 'filename' => get_user_static_domain() . $folderName .'/'. $safeName];
+        return $folderName .'/'. $safeName;
     }
 }
