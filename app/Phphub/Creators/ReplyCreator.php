84aef313c587c918d208dfe8ae8eb76dd2260043
Summer <summer@yousails.com>
2016-07-21 09:38:38 +0800

    fixing email notification sended bug

--- app/Phphub/Creators/ReplyCreator.php
+++ app/Phphub/Creators/ReplyCreator.php
@@ -47,7 +47,7 @@ class ReplyCreator
 
         app('Phphub\Notification\Notifier')->newReplyNotify(Auth::user(), $this->mentionParser, $topic, $reply);
 
-        if (Auth::user()->email_notify_enabled == 'yes' && $topic->user_id != Auth::id()) {
+        if ($topic->user->email_notify_enabled == 'yes' && $topic->user_id != Auth::id()) {
             dispatch(new SendReplyNotifyMail($reply));
         }
 
