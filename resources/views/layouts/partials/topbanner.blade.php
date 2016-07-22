09b5415c794193b42e7bcd9386bd156a3b9ef975
Charlie Jade <cj@estgroupe.com>
2016-07-23 06:00:07 +0800

    topbanner style

--- resources/views/layouts/partials/topbanner.blade.php
+++ resources/views/layouts/partials/topbanner.blade.php
@@ -4,7 +4,7 @@
     <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
         <div class="item">
             <a href="@if($banner->link){{$banner->link}}@else javascript:; @endif" target="{{$banner->target}}">
-                <p class="img"><span style="background-image:url({{$banner->image_url}})"></span></p>
+                <p class="img"><span style="background-image:url({{$banner->image_url}}?imageView2/1/w/424/h/212)"></span></p>
                 <p class="caption">{{$banner->title}}</p>
             </a>
         </div>
