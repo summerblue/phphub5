c26cae310970bc4c9fcf64e06b621c4a297180b8
Charlie Jade <cj@estgroupe.com>
2016-07-24 07:18:32 +0800

    change sites item style

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -11,13 +11,17 @@
         欢迎交换友链，请加我微信 <i class="fa fa-weixin text-md"></i> summer_charlie，只接受 PHP、Laravel 相关话题的站点，如果微信回复较慢可以私信 <i class="fa fa-weibo text-md"></i> <a href="http://weibo.com/phphub">PHPHub</a>
     </div>
 
-    @include('sites.partials.sites', ['heading_title' => '<i class="fa fa-weibo text-md"></i> Laravel/PHP 圈内人的微博', 'filterd_sites' => $sites['weibo']])
+    <div class="sites-index">
 
-    @include('sites.partials.sites', ['heading_title' => '<i class="fa fa-globe text-md"></i> 推荐网站', 'filterd_sites' => $sites['site']])
+        @include('sites.partials.sites', ['heading_title' => '<i class="fa fa-weibo text-md"></i> Laravel/PHP 圈内人的微博', 'filterd_sites' => $sites['weibo']])
 
-    @include('sites.partials.sites', ['heading_title' => '<i class="fa fa-flag text-md"></i> 推荐博客', 'filterd_sites' => $sites['blog']])
+        @include('sites.partials.sites', ['heading_title' => '<i class="fa fa-globe text-md"></i> 推荐网站', 'filterd_sites' => $sites['site']])
 
-    @include('sites.partials.sites', ['heading_title' => '<i class="fa fa-cloud text-md"></i> 开发者服务推荐', 'filterd_sites' => $sites['dev_service']])
+        @include('sites.partials.sites', ['heading_title' => '<i class="fa fa-flag text-md"></i> 推荐博客', 'filterd_sites' => $sites['blog']])
+
+        @include('sites.partials.sites', ['heading_title' => '<i class="fa fa-cloud text-md"></i> 开发者服务推荐', 'filterd_sites' => $sites['dev_service']])
+
+    </div>
 
     @include('layouts.partials.topbanner')
 
