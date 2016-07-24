f06efd109ce16efd57fa2909de825fcc544b5c8c
Charlie Jade <cj@estgroupe.com>
2016-07-24 21:33:22 +0800

    fix small screen footer sticked problem

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -13,14 +13,14 @@
           <div class="col-sm-6 col-lg-6 col-lg-offset-1">
 
               <div class="row">
-                <div class="col-xs-3">
+                <div class="col-sm-3">
                   <h4>赞助商</h4>
                   <ul class="list-unstyled">
                     <a href="http://www.ucloud.cn/?utm_source=zanzhu&utm_campaign=phphub&utm_medium=display&utm_content=yejiao&ytag=phphubyejiao" target="_blank"><img src="https://dn-phphub.qbox.me/uploads/images/201605/03/1/dYfOYswiQY.png" class="popover-with-html" width="98" data-placement="top" data-content="本站服务器由 UCloud 赞助"></a>
                   </ul>
                 </div>
 
-                  <div class="col-xs-3">
+                  <div class="col-sm-3">
                     <h4>{{ lang('Site Status') }}</h4>
                     <ul class="list-unstyled">
                         <li>{{ lang('Total User') }}: {{ $siteStat->user_count }} </li>
@@ -28,7 +28,7 @@
                         <li>{{ lang('Total Reply') }}: {{ $siteStat->reply_count }} </li>
                     </ul>
                   </div>
-                  <div class="col-xs-3">
+                  <div class="col-sm-3">
                     <h4>其他信息</h4>
                     <ul class="list-unstyled">
                         <li><a href="/about">关于我们</a></li>
@@ -37,7 +37,7 @@
                         <li class="popover-with-html" data-content="新手 QQ 群">Q 群：579866868</li>
                     </ul>
                   </div>
-                  <div class="col-xs-3">
+                  <div class="col-sm-3">
                       <h4>微信订阅号</h4>
                     <img class="image-border popover-with-html" data-content="扫码，或者搜索微信订阅号：PHPHub" src="https://dn-phphub.qbox.me/uploads/images/201607/19/1/5KAh3F8EhD.jpg" style="width:100px;height:100px;">
                   </div>
