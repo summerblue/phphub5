7f695bae9017f93f436fd5d469bd7bc02b3b8c8b
Charlie Jade <cj@estgroupe.com>
2016-07-27 05:42:08 +0800

    update composing hint

--- resources/views/topics/partials/composing_help_block.blade.php
+++ resources/views/topics/partials/composing_help_block.blade.php
@@ -2,10 +2,8 @@
     <ul class="helpblock list">
       <li>{!! lang('publish_typography') !!}</li>
       <li>{!! lang('publish_markdown') !!}</li>
-      <li>{!! lang('publish_emoji') !!}</li>
       <li>支持表情，使用方法请见 <a href="https://phphub.org/topics/45" target="_blank">Emoji 自动补全来咯</a>，可用的 Emoji 请见 <a href="https://phphub.org/ecc/index.html" target="_blank" rel="nofollow">Emoji 列表</a></li>
       <li>{!! lang('publish_image') !!}</li>
       <li>{!! lang('publish_draft') !!}</li>
-      <li>招聘话题相关请严格按照 - <a href="https://phphub.org/topics/817" target="_blank">PHPHub 招聘贴发布规范</a></li>
     </ul>
 </div>
