36d60a99f0a98c4301ab4cf132fafdb17b35cddd
zhengjinghua <jin114001251@gmail.com>
2016-07-28 23:05:01 +0800

    Add clear user data logic

---
+++ app/Console/Commands/ClearUserData.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Topic;
+use App\Models\Reply;
+use App\Models\Notification;
+use App\Models\Attention;
+use App\Models\Favorite;
+
+class ClearUserData extends Command
+{
+
+    protected $signature = 'phphub:clear-user-data {user_id}';
+
+
+    protected $description = 'Clear user data
+                            {user_id : User ID }
+                            ';
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $user_id = $this->argument('user_id');
+
+        Topic::where('user_id', $user_id)->delete();
+        Reply::where('user_id', $user_id)->delete();
+        Notification::where('user_id', $user_id)->delete();
+        Attention::where('user_id', $user_id)->delete();
+        Favorite::where('user_id', $user_id)->delete();
+    }
+}
