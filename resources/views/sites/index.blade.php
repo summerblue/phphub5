8f6594b30cf47c69bb20be30d53d539f0a47b17d
Summer <summer@yousails.com>
2016-07-29 12:19:09 +0800

    update sites intro

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -8,7 +8,7 @@
 
     <div class="box text-center site-intro rm-link-color">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        欢迎交换友链，提交请见 <a style="text-decoration: underline;" href="https://phphub.org/topics/2453">关于酷站</a>
+        欢迎交换友链，只接受 PHP、Laravel 相关话题的站点，请见 <a style="text-decoration: underline;" href="https://phphub.org/topics/2453">关于酷站</a>
     </div>
 
     <div class="sites-index">
