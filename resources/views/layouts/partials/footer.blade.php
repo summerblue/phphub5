7a45938624af09133b5de258bef4b7f18efca0bc
zhengjinghua <jin114001251@gmail.com>
2016-07-29 22:55:49 +0800

    Add footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -5,6 +5,12 @@
           <div class="col-sm-5 col-lg-5">
               <h4>
                   <img src="https://dn-phphub.qbox.me/assets/images/banner_transparent.png" alt="" width="160"/>
+                  <span class="pull-right text-md padding-top-lg" style="padding-top: 18px;">
+                      <a class="popover-with-html" data-content="查看源代码" target="_blank" style="padding-right:8px" href="https://github.com/summerblue/phphub5"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
+                      <a class="popover-with-html" data-content="关注 PHPHub 微博账号" target="_blank" style="padding-right:8px" href="http://weibo.com/phphub"><i class="fa fa-weibo" aria-hidden="true"></i></a>
+                      <a class="popover-with-html" data-content="扫码关注微信订阅号：PHPHub" target="_blank" style="padding-right:8px" href="https://dn-phphub.qbox.me/uploads/images/201607/19/1/5KAh3F8EhD.jpg"><i class="fa fa-weixin" aria-hidden="true"></i></a>
+                      <a class="popover-with-html" data-content="下载 Chrome 消息通知插件" target="_blank" style="padding-right:8px" href="https://chrome.google.com/webstore/detail/phphub-notifier/fcopfkdgikhodlcjkjdppdfkbhmehdon"><i class="fa fa-chrome" aria-hidden="true"></i></a>
+                  </span>
               </h4>
               <p class="padding-top-xsm">我们是国内最大的 PHP &amp; Laravel 开源社区，致力于打造一个 PHPer 分享创造、结识伙伴、协同互助的平台。</p>
               <span style="font-size:0.9em">♥ Do what you love, love what you do. - <a href="http://estgroupe.com/">The EST Group</a></span>
