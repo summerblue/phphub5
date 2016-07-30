544ba60c75c0c29d41d61e51c1208c18bd8a6b89
zhengjinghua <jin114001251@gmail.com>
2016-07-30 08:00:07 +0800

    Update calculate hot topic logic

--- app/Console/Commands/CalculateHotTopic.php
+++ app/Console/Commands/CalculateHotTopic.php
@@ -8,6 +8,7 @@ use App\Models\HotTopic;
 use App\Models\Favorite;
 use App\Models\Attention;
 use App\Models\Vote;
+use App\Models\Topic;
 use Carbon\Carbon;
 use DB;
 
@@ -29,53 +30,24 @@ class CalculateHotTopic extends Command
     public function handle()
     {
         HotTopic::query()->delete();
-
-        $this->calculateVoteTopics();
-        $this->calculateReplyTopics();
-        $this->calculateWeight();
+        $this->calculateTopics();
     }
 
-    public function calculateVoteTopics()
+    public function calculateTopics()
     {
-        $topics = Vote::query()->select(DB::raw('votable_id, count(*) as vote_count'))
-                               ->where('votable_type', 'Topic')
-                               ->where('is', 'upvote')
-                               ->where('created_at', '>=', Carbon::now()->subDays(self::PASS_DAYS))
-                               ->groupBy('votable_id')
-                               ->get();
-
-        foreach ($topics as $value) {
+        $topics = Topic::where('created_at', '>=', Carbon::now()->subDays(self::PASS_DAYS))->get();
+        foreach ($topics as $topic) {
             $data = [];
-            $data['topic_id'] = $value->votable_id;
-            $data['vote_count'] = $value->vote_count;
+            $data['topic_id']    = $topic->id;
+            $data['reply_count'] = Reply::where('topic_id', $topic->id)->count();
+            $data['vote_count']  = Vote::where('votable_type', 'Topic')
+                                       ->where('votable_id', $topic->id)
+                                       ->count();
 
-            HotTopic::updateOrCreate(['topic_id' => $value->topic_id], $data);
-        }
-    }
+            $data['weight'] = $data['vote_count'] * self::VOTE_TOPIC_WEIGHT
+                            + $data['reply_count'] * self::REPLY_TOPIC_WEIGHT;
 
-    public function calculateReplyTopics()
-    {
-        $topics = Reply::query()->select(DB::raw('topic_id, count(*) as reply_count'))
-                                     ->where('created_at', '>=', Carbon::now()->subDays(self::PASS_DAYS))
-                                     ->groupBy('topic_id')
-                                     ->get();
-
-        foreach ($topics as $value) {
-            $data = [];
-            $data['topic_id'] = $value->topic_id;
-            $data['reply_count'] = $value->reply_count;
-
-            HotTopic::updateOrCreate(['topic_id' => $value->topic_id], $data);
-        }
-    }
-
-    public function calculateWeight()
-    {
-        $hot_topics = HotTopic::all();
-        foreach ($hot_topics as $hot_topic) {
-            $hot_topic->weight = $hot_topic->vote_count * self::VOTE_TOPIC_WEIGHT
-                                 + $hot_topic->reply_count * self::REPLY_TOPIC_WEIGHT;
-            $hot_topic->save();
+            HotTopic::updateOrCreate(['topic_id' => $topic->id], $data);
         }
     }
 }
