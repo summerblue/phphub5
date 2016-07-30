2da9b022385b771965b31ff9ad969b6253f94a52
zhengjinghua <jin114001251@gmail.com>
2016-07-30 12:54:19 +0800

    Add session for redis

--- config/database.php
+++ config/database.php
@@ -122,6 +122,12 @@ return [
             'database' => 0,
         ],
 
+        'session' => [
+              'host'     => env('REDIS_HOST', 'localhost'),
+              'password' => env('REDIS_PASSWORD', null),
+              'port'     => env('REDIS_PORT', 6379),
+              'database' => 1,
+          ],
     ],
 
 ];
