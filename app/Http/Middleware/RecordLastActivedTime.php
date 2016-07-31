709272f48f74efd82967ff43e50cf01895aa4c60
zhengjinghua <jin114001251@gmail.com>
2016-07-31 21:49:49 +0800

    Improve logic

--- app/Http/Middleware/RecordLastActivedTime.php
+++ app/Http/Middleware/RecordLastActivedTime.php
@@ -4,12 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Auth;
+use Request;
 
 class RecordLastActivedTime
 {
     public function handle($request, Closure $next)
     {
-        if (Auth::check()) {
+        if (Auth::check() && Request::is('notifications/count') == false) {
             Auth::user()->recordLastActivedAt();
         }
 
