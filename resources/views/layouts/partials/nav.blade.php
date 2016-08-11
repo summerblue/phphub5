a802019bedd926572a6cd378b7d9bae7538436a0
Charlie Jade <cj@estgroupe.com>
2016-08-11 22:35:50 +0800

    refactoring top nav

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -23,38 +23,54 @@
         <ul class="nav navbar-nav github-login" >
           @if (Auth::check())
               <li>
+                  <a href="{{ isset($category) ? URL::route('topics.create', ['category_id' => $category->id]) : URL::route('topics.create') }}" data-placement="bottom" class="popover-with-html" data-content="添加主题" style="margin-top: 3px;">
+                      <i class="fa fa-plus text-md"></i>
+                  </a>
+              </li>
+
+              <li>
                   <a href="{{ route('notifications.index') }}" class="text-warning">
-                      <span class="badge badge-{{ $currentUser->notification_count > 0 ? 'important' : 'fade' }}" id="notification-count">
+                      <span class="badge badge-{{ $currentUser->notification_count > 0 ? 'important' : 'fade' }} popover-with-html" data-content="消息提醒" id="notification-count">
                           {{ $currentUser->notification_count }}
                       </span>
                   </a>
               </li>
 
-                @if (Auth::user()->can('visit_admin'))
-                  <li>
-                      <a href="/admin" class="text-md  ">
-                          <i class="fa fa-tachometer"></i>
-                      </a>
-                  </li>
-                @endif
-                <li>
-                    <a class="nav-edit-btn text-md margin-top-2px" href="{{ route('users.edit', $currentUser->id) }}" data-content="{{ lang('Edit Profile') }}">
-                        <i class="fa fa-cog"></i>
-                    </a>
-                </li>
-
-
               <li>
-                  <a href="{{ route('users.show', $currentUser->id) }}">
+                  <a href="#" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dLabel" >
                       <img class="avatar-topnav" alt="Summer" src="{{ $currentUser->present()->gravatar }}" />
                        {{{ $currentUser->name }}}
+                       <span class="caret"></span>
                   </a>
+
+                  <ul class="dropdown-menu" aria-labelledby="dLabel">
+
+                      @if (Auth::user()->can('visit_admin'))
+                        <li>
+                            <a href="/admin">
+                                <i class="fa fa-tachometer text-md"></i> 管理后台
+                            </a>
+                        </li>
+                      @endif
+
+                      <li>
+                          <a class="button" href="{{ route('users.show', $currentUser->id) }}" data-lang-loginout="{{ lang('Are you sure want to logout?') }}">
+                              <i class="fa fa-user text-md"></i> 个人中心
+                          </a>
+                      </li>
+                      <li>
+                          <a class="button" href="{{ route('users.edit', $currentUser->id) }}" >
+                              <i class="fa fa-cog text-md"></i> 个人资料
+                          </a>
+                      </li>
+                      <li>
+                          <a id="login-out" class="button" href="{{ URL::route('logout') }}" data-lang-loginout="{{ lang('Are you sure want to logout?') }}">
+                              <i class="fa fa-sign-out text-md"></i> {{ lang('Logout') }}
+                          </a>
+                      </li>
+                  </ul>
               </li>
-              <li>
-                  <a id="login-out" class="button" href="{{ URL::route('logout') }}" data-lang-loginout="{{ lang('Are you sure want to logout?') }}">
-                      <i class="fa fa-sign-out"></i> {{ lang('Logout') }}
-                  </a>
-              </li>
+
           @else
               <a href="{{ URL::route('auth.oauth', ['driver' => 'wechat']) }}" class="btn btn-success login-btn weichat-login-btn">
                 <i class="fa fa-weixin"></i>
