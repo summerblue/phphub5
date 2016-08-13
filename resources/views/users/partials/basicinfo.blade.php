25f29ca5e59e738cd1aacceccc66bfb3fa2fdba7
Charlie Jade <cj@estgroupe.com>
2016-08-14 06:32:59 +0800

    change user info order

--- resources/views/users/partials/basicinfo.blade.php
+++ resources/views/users/partials/basicinfo.blade.php
@@ -55,12 +55,12 @@
               <a class="text" href="{{ route('users.followers', $user->id) }}">关注者</a>
             </div>
             <div class="col-xs-4">
-              <a class="counter" href="{{ route('users.topics', $user->id) }}">{{ $user->topic_count }}</a>
-              <a class="text" href="{{ route('users.topics', $user->id) }}">话题</a>
+                <a class="counter" href="{{ route('users.replies', $user->id) }}">{{ $user->reply_count }}</a>
+                <a class="text" href="{{ route('users.replies', $user->id) }}">评论</a>
             </div>
             <div class="col-xs-4">
-              <a class="counter" href="{{ route('users.replies', $user->id) }}">{{ $user->reply_count }}</a>
-              <a class="text" href="{{ route('users.replies', $user->id) }}">评论</a>
+                <a class="counter" href="{{ route('users.topics', $user->id) }}">{{ $user->topic_count }}</a>
+                <a class="text" href="{{ route('users.topics', $user->id) }}">话题</a>
             </div>
         </div>
 
