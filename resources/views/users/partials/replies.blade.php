9eaa6d8ac4c09f5e96136d790dfd44071514f6f6
Charlie Jade <cj@estgroupe.com>
2016-08-25 09:37:55 +0800

    block banned users replies

--- resources/views/users/partials/replies.blade.php
+++ resources/views/users/partials/replies.blade.php
@@ -10,12 +10,18 @@
       <span class="meta">
          at <span class="timeago" title="{{ $reply->created_at }}">{{ $reply->created_at }}</span>
       </span>
+
+        @if ($reply->is_blocked != 'yes')
       <div class="reply-body markdown-reply content-body">
 {!! $reply->body !!}
       </div>
+        @else
+        <div class="deleted text-center">内容被屏蔽</div>
+        @endif
     @else
       <div class="deleted text-center">{{ lang('Data has been deleted.') }}</div>
     @endif
+
 
   </li>
   @endforeach
