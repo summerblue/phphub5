764c41e27317e29c8b2805068a3823f3f0d919c0
Summer <summer@yousails.com>
2016-09-08 13:18:56 +0800

    update qrcode

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -7,8 +7,8 @@
                   <img src="https://dn-phphub.qbox.me/assets/images/banner_transparent.png" alt="" width="160"/>
                   <span class="pull-right text-md padding-top-lg" style="padding-top: 18px;">
                       <a class="popover-with-html" data-content="查看源代码" target="_blank" style="padding-right:8px" href="https://github.com/summerblue/phphub5"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
-                      <a class="popover-with-html" data-content="关注 Laravel 资讯" target="_blank" style="padding-right:8px" href="http://weibo.com/laravelnews"><i class="fa fa-weibo" aria-hidden="true"></i></a>
-                      <a class="popover-with-html" data-content="扫码关注微信订阅号：PHPHub" target="_blank" style="padding-right:8px" href="https://dn-phphub.qbox.me/uploads/images/201607/19/1/5KAh3F8EhD.jpg"><i class="fa fa-weixin" aria-hidden="true"></i></a>
+                      <a class="popover-with-html" data-content="关注「Laravel资讯」" target="_blank" style="padding-right:8px" href="http://weibo.com/laravelnews"><i class="fa fa-weibo" aria-hidden="true"></i></a>
+                      <a class="popover-with-html" data-content="扫码关注微信订阅号：「Laravel资讯」" target="_blank" style="padding-right:8px" href="https://dn-phphub.qbox.me/uploads/images/201607/19/1/5KAh3F8EhD.jpg"><i class="fa fa-weixin" aria-hidden="true"></i></a>
                       <a class="popover-with-html" data-content="下载 Chrome 消息通知插件" target="_blank" style="padding-right:8px" href="https://chrome.google.com/webstore/detail/phphub-notifier/fcopfkdgikhodlcjkjdppdfkbhmehdon"><i class="fa fa-chrome" aria-hidden="true"></i></a>
                   </span>
               </h4>
@@ -50,7 +50,7 @@
                   </div>
                   <div class="col-sm-3">
                       <h4>微信订阅号</h4>
-                    <img class="image-border popover-with-html" data-content="扫码，或者搜索微信订阅号：PHPHub" src="https://dn-phphub.qbox.me/uploads/images/201607/19/1/5KAh3F8EhD.jpg" style="width:100px;height:100px;">
+                    <img class="image-border popover-with-html" data-content="扫码，或者搜索微信订阅号：「Laravel资讯」" src="https://dn-phphub.qbox.me/uploads/images/201609/08/1/43cy0uEmy0.jpg" style="width:100px;height:100px;">
                   </div>
 
                 </div>
