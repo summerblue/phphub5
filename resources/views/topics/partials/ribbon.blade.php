027d783df5d2bbaee215e9573be232410f9d7c09
Summer <summer@yousails.com>
2016-09-09 14:04:28 +0800

    add anchored warning

--- resources/views/topics/partials/ribbon.blade.php
+++ resources/views/topics/partials/ribbon.blade.php
@@ -1,11 +1,16 @@
 
 @if ($topic->is_excellent == 'yes')
   <div class="ribbon">
-    @if ($topic->is_excellent == 'yes')
       <div class="ribbon-excellent">
           <i class="fa fa-trophy"></i> {{ lang('This topic has been mark as Excenllent Topic.') }}
       </div>
-    @endif
+  </div>
+@endif
 
+@if ($topic->order == -1)
+  <div class="ribbon">
+      <div class="ribbon-anchored">
+          <i class="fa fa-anchor"></i> 此贴已被下沉，请查看 <a href="https://phphub.org/topics/2802">下沉说明</a> 进行修改。
+      </div>
   </div>
 @endif
