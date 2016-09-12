248dc9d6fdae6fde001430fa5e1d34ef001a06f3
Summer <summer@yousails.com>
2016-09-12 22:11:55 +0800

    update links

--- resources/views/topics/partials/composing_help_block.blade.php
+++ resources/views/topics/partials/composing_help_block.blade.php
@@ -2,7 +2,7 @@
     <ul class="helpblock list">
       <li>{!! lang('publish_typography') !!}</li>
       <li>{!! lang('publish_markdown') !!}</li>
-      <li>支持表情，使用方法请见 <a href="https://phphub.org/topics/45" target="_blank">Emoji 自动补全来咯</a>，可用的 Emoji 请见 :metal: :point_right: <a href="https://phphub.org/ecc/index.html" target="_blank" rel="nofollow"> Emoji 列表 </a> :star: :sparkles: </li>
+      <li>支持表情，使用方法请见 <a href="http://phphub.laravel-china.org/topics/45" target="_blank">Emoji 自动补全来咯</a>，可用的 Emoji 请见 :metal: :point_right: <a href="http://phphub.laravel-china.org/ecc/index.html" target="_blank" rel="nofollow"> Emoji 列表 </a> :star: :sparkles: </li>
       <li>{!! lang('publish_image') !!}</li>
       <li>{!! lang('publish_draft') !!}</li>
     </ul>
