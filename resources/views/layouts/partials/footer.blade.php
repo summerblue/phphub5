6a175726d8ecb9ddb89e94222c78a444a4218e9a
Charlie Jade <cj@estgroupe.com>
2016-09-14 07:30:26 +0800

    update logo

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,16 +3,16 @@
         <div class="row footer-top">
 
           <div class="col-sm-5 col-lg-5">
-              <h4>
-                  <img src="https://dn-phphub.qbox.me/assets/images/banner_transparent.png" alt="" width="160"/>
-                  <span class="pull-right text-md padding-top-lg" style="padding-top: 18px;">
-                      <a class="popover-with-html" data-content="查看源代码" target="_blank" style="padding-right:8px" href="https://github.com/summerblue/phphub5"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
-                      <a class="popover-with-html" data-content="关注「Laravel资讯」" target="_blank" style="padding-right:8px" href="http://weibo.com/laravelnews"><i class="fa fa-weibo" aria-hidden="true"></i></a>
-                      <a class="popover-with-html" data-content="扫码关注微信订阅号：「Laravel资讯」" target="_blank" style="padding-right:8px" href="https://dn-phphub.qbox.me/uploads/images/201607/19/1/5KAh3F8EhD.jpg"><i class="fa fa-weixin" aria-hidden="true"></i></a>
-                      <a class="popover-with-html" data-content="下载 Chrome 消息通知插件" target="_blank" style="padding-right:8px" href="https://chrome.google.com/webstore/detail/phphub-notifier/fcopfkdgikhodlcjkjdppdfkbhmehdon"><i class="fa fa-chrome" aria-hidden="true"></i></a>
-                  </span>
-              </h4>
-              <p class="padding-top-xsm">我们是中国最靠谱的 PHP &amp; Laravel 开发者社区，致力于打造一个 PHPer 分享创造、结识伙伴、协同互助的平台。</p>
+
+              <p class="padding-top-xsm">我们是中国最靠谱的 Laravel 开发者社区，致力于为 Laravel 开发者提供一个分享创造、结识伙伴、协同互助的平台。</p>
+
+              <div class="text-md " >
+                  <a class="popover-with-html" data-content="查看源代码" target="_blank" style="padding-right:8px" href="https://github.com/summerblue/phphub5"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
+                  <a class="popover-with-html" data-content="关注「Laravel资讯」" target="_blank" style="padding-right:8px" href="http://weibo.com/laravelnews"><i class="fa fa-weibo" aria-hidden="true"></i></a>
+                  <a class="popover-with-html" data-content="扫码关注微信订阅号：「Laravel资讯」" target="_blank" style="padding-right:8px" href="https://dn-phphub.qbox.me/uploads/images/201607/19/1/5KAh3F8EhD.jpg"><i class="fa fa-weixin" aria-hidden="true"></i></a>
+                  <a class="popover-with-html" data-content="下载 Chrome 消息通知插件" target="_blank" style="padding-right:8px" href="https://chrome.google.com/webstore/detail/phphub-notifier/fcopfkdgikhodlcjkjdppdfkbhmehdon"><i class="fa fa-chrome" aria-hidden="true"></i></a>
+              </div>
+              <br>
               <span style="font-size:0.9em">
                   Powered by <a href="https://github.com/summerblue/phphub5" target="_blank" style="color:inherit">PHPHub</a>
               </span>
@@ -21,7 +21,7 @@
           <div class="col-sm-6 col-lg-6 col-lg-offset-1">
 
               <div class="row">
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                   <h4>赞助商</h4>
                   <ul class="list-unstyled">
                       @if(isset($banners['footer-sponsor']))
@@ -32,7 +32,7 @@
                   </ul>
                 </div>
 
-                  <div class="col-sm-3">
+                  <div class="col-sm-4">
                     <h4>{{ lang('Site Status') }}</h4>
                     <ul class="list-unstyled">
                         <li>{{ lang('Total User') }}: {{ $siteStat->user_count }} </li>
@@ -40,17 +40,13 @@
                         <li>{{ lang('Total Reply') }}: {{ $siteStat->reply_count }} </li>
                     </ul>
                   </div>
-                  <div class="col-sm-3">
+                  <div class="col-sm-4">
                     <h4>其他信息</h4>
                     <ul class="list-unstyled">
                         <li><a href="/about">关于我们</a></li>
                         <li><a href="{{ route('hall_of_fames') }}"><i class="fa fa-star" aria-hidden="true"></i> {{ lang('Hall of Fame') }}</a></li>
                         <li class="popover-with-html" data-content="新手 QQ 群">Q 群：579866868</li>
                     </ul>
-                  </div>
-                  <div class="col-sm-3">
-                      <h4>微信订阅号</h4>
-                    <img class="image-border popover-with-html" data-content="扫码，或者搜索微信订阅号：「Laravel资讯」" src="https://dn-phphub.qbox.me/uploads/images/201609/08/1/43cy0uEmy0.jpg" style="width:100px;height:100px;">
                   </div>
 
                 </div>
--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -3,7 +3,7 @@
     <div class="navbar-header hidden-xs">
 
       <a href="/" class="navbar-brand">
-          <img src="{{ cdn('assets/images/logo2.png') }}" alt="PHPHub" />
+          <img src="{{ cdn('assets/images/logo3.png') }}" alt="PHPHub" />
       </a>
     </div>
     <div id="top-navbar-collapse" class="navbar-collapse">
