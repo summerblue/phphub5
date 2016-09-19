0a9a455144abc789b25875801dda49f899fcd956
Summer <summer@yousails.com>
2016-09-19 16:38:34 +0800

    more replies on one page

--- config/phphub.php
+++ config/phphub.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'replies_perpage'         => 80,
+    'replies_perpage'         => 200,
     'actived_time_for_update' => 'actived_time_for_update',
     'actived_time_data'       => 'actived_time_data',
 ];
