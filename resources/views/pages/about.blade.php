44a85f5ae2e9788955c3c93791946e6b2b0d64fc
Summer <summer@yousails.com>
2016-10-06 11:00:56 +0800

    clean up

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -12,29 +12,29 @@
         <div class="markdown-body" id="emojify">
 
 
-<h1 id="toc_0">关于 PHPHub</h1>
+<h1 id="toc_0">关于 Laravel China</h1>
 
 <h2 id="toc_1">说明</h2>
 
-<p>Laravel China 积极向上的 Laravel 和 开发者社区</p>
+<p>Laravel China 积极向上的 Laravel 和 PHP 开发者社区</p>
 <p>我们致力于推动 Laravel, php-fig 等国外 PHP 新技术，新理念在中国的发展。</p>
 
-<p>论坛基于开源同名项目 PHPHub，PHPHub 项目 MIT Licence 开源，由 Laravel5.1 构建，<a href="https://github.com/summerblue/phphub5">源代码在此 at GitHub</a>。欢迎贡献代码，或者 <a href="https://github.com/summerblue/phphub5/issues">提交 Bug Report</a>。</p>
+<p>论坛基于开源项目 PHPHub，PHPHub 由 Laravel5.1 构建，<a href="https://github.com/summerblue/phphub5">源代码在此 at GitHub</a>。欢迎贡献代码，或者 <a href="https://github.com/summerblue/phphub5/issues">提交 Bug Report</a>。</p>
 
 <h2 id="toc_2">关于新手问题</h2>
 
 <p>这个社区不是用来问新手问题的，如果你是 PHP 新手，请先看 <a href="http://www.phptherightway.com/">PHP The Right Way</a>， 里面有中文版本，此文档能为你构建一个合理的 PHP 基础知识体系。</p>
 
-<p>第一次接触 Laravel? 这里是  <a href="http://laravel-china.org/">Laravel 中文文档</a>，Laravel 的出名，也得益于这份撰写精炼的文档，值得多次品读。</p>
+<p>第一次接触 Laravel? 这里是  <a href="http://laravel-china.org/docs/home">Laravel 中文文档</a>，Laravel 的出名，也得益于这份撰写精炼的文档，值得多次品读。</p>
 
 <p>在学习上遇到问题的时候，请先 Google。</p>
 
-<p>如果觉得你的问题比较独占，请先仔细阅读 <a href="https://laravel-china.org/topics/535">提问的智慧</a>，然后在 <a href="https://laravel-china.org/categories/4">PHPHub 问答分类下</a> 发表你的问题，谢谢。 </p>
+<p>如果觉得你的问题比较独占，请先仔细阅读 <a href="https://laravel-china.org/topics/535">提问的智慧</a>，然后在 <a href="https://laravel-china.org/categories/4">问答分类下</a> 发表你的问题，谢谢。 </p>
 
 <h2 id="toc_3">愿景 Vision</h2>
 
 <blockquote>
-<p>下面是 phphub.org 创建的初衷, 与君共勉. </p>
+<p>下面是 laravel-china.org 创建的初衷，与君共勉。</p>
 </blockquote>
 
 <h3 id="toc_4">在这里的我们</h3>
@@ -80,12 +80,6 @@
 <p><a href="http://www.zhihu.com/question/19558755/answer/23062110">知乎上的答案 - 最好的 PHP 框架是什么？为什么？</a></p>
 
 <p>需要补充的一句是, <code>绝不排斥</code> 别的框架的讨论, 只要是 <code>PHP</code> 相关技术讨论我们都欢迎。</p>
-
-<h2 id="toc_8">Logo 和 icon</h2>
-
-<p><img src="{{ cdn('assets/images/banner_transparent.png') }}" alt=""></p>
-
-<p><img src="{{ cdn('assets/images/favicon.png') }}" alt=""></p>
 
 
 
