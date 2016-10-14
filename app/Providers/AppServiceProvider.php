bd5651ad7d631c5d91dac305fdebbbe8c4d4f4a4
zhengjinghua <jin114001251@gmail.com>
2016-10-14 17:21:04 +0800

    Add admin operation history in topic's detail view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Carbon::setLocale('zh');
     }
 
     /**
