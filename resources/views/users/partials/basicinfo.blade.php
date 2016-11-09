4fad0616657344c43d2667e1701e866e3e3ce9a7
zhengjinghua <jin114001251@gmail.com>
2016-11-09 17:34:00 +0800

    Improve admin operation

--- resources/views/users/partials/basicinfo.blade.php
+++ resources/views/users/partials/basicinfo.blade.php
@@ -143,7 +143,7 @@
         </a>
         @endif
 
-        @if ($currentUser && Entrust::can('manage_users') && ($currentUser->id != $user->id))
+        @if ($currentUser && Entrust::can('manage_users') && ($currentUser->id != $user->id) && $currentUser->roles->count() < 5)
           <a data-method="post" class="btn btn-{{ $user->is_banned == 'yes' ? 'warning' : 'danger' }} btn-block" href="javascript:void(0);" data-url="{{ route('users.blocking', $user->id) }}" id="user-edit-button" onclick=" return confirm('{{ lang('Are you sure want to '. ($user->is_banned == 'yes' ? 'unblock' : 'block') . ' this User?') }}')">
             <i class="fa fa-times"></i> {{ $user->is_banned == 'yes' ? lang('Unblock User') : lang('Block User') }}
           </a>
