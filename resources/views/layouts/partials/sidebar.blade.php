f2231d0d3129dfad34f027952d350cc868a7cbf2
Summer <cj@estgroupe.com>
2016-12-09 11:24:08 +0800

    add banner

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -67,8 +67,17 @@
     background-color: transparent;
     border: none;
 ">
-<a href="https://laravel-china.org/topics/3383" target="_blank" rel="nofollow" title="" style="">
-      <img src="https://dn-phphub.qbox.me/uploads/images/201612/09/1/vGTXfKk57y.jpg" style="width: 100%;border-radius: 0px;box-shadow: none;border: 1px solid #e2e1e1;"></a>
+<a href="https://laravel-china.org/topics/3383" rel="nofollow" title="" style="">
+      <img src="https://dn-phphub.qbox.me/uploads/images/201612/09/1/qASrRyKNj0.jpg" style="width: 100%;border-radius: 0px;box-shadow: none;border: 1px solid #ffafaf;"></a>
+</div>
+
+<div class="panel panel-default corner-radius" style="
+    text-align: center;
+    background-color: transparent;
+    border: none;
+">
+<a href="https://laravel-china.org/topics/3213" rel="nofollow" title="" style="">
+      <img src="https://dn-phphub.qbox.me/uploads/images/201612/09/1/06fkozmGAH.jpg" style="width: 100%;border-radius: 0px;box-shadow: none;border: 1px solid #e2e1e1;"></a>
 </div>
 
 
