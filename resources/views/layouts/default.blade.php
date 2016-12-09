58cd7127553be1e14ad37b4803523b0a5a5e07b7
Summer <cj@estgroupe.com>
2016-12-09 15:19:33 +0800

    analytics localize

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -89,11 +89,6 @@ ______                            _              _                              
 
         @if (App::environment() == 'production')
 		<script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
           ga('create', '{{ getenv('GA_Tracking_ID') }}', 'auto');
           ga('send', 'pageview');
         </script>
