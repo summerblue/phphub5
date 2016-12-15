78bf3b6f271a34cadddfb2c79fc6b35cd4ead324
Summer <cj@estgroupe.com>
2016-12-15 14:36:12 +0800

    filter notification

--- resources/views/topics/partials/body.blade.php
+++ resources/views/topics/partials/body.blade.php
@@ -6,4 +6,5 @@
     <a class="popover-with-html" data-content="作者署名" target="_blank" style="display: block;width: 30px;color: #ccc;margin: 22px 0 8px;" href="https://laravel-china.org/topics/3422"><i class="fa fa-paw" aria-hidden="true"></i></a>
     {!! $topic->user->present()->formattedSignature() !!}
 @endif
+
 </div>
