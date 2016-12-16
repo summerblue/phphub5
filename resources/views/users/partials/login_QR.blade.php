dc2faad5ca52b8b66ebb2eb5943d62f615720822
Summer <summer@yousails.com>
2016-12-16 10:12:22 +0800

    update qrcode hint

--- resources/views/users/partials/login_QR.blade.php
+++ resources/views/users/partials/login_QR.blade.php
@@ -4,7 +4,8 @@
   <br/><br/>
 <form method="POST" action="{{ route('users.regenerate_login_token') }}" accept-charset="UTF-8">
     {!! csrf_field() !!}
-    <input class="btn btn-sm btn-default" id="topic-create-submit" type="submit" value="重新生成">
+    <div style="margin-bottom: 8px;color: #999;font-size: 0.9em;">登录失败请点击</div>
+    <input class="btn btn-sm btn-default" style="margin-bottom: 13px;" id="topic-create-submit" type="submit" value="重新生成">
 </form>
 
 </div>
