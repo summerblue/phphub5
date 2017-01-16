1c72bbab1ad9293e3774bf643cdb30df59c00e38
Summer <cj@estgroupe.com>
2017-01-16 12:16:42 +0800

    mobile style

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,7 +8,43 @@
 
 <div class="blog-pages">
 
-      <div class="col-md-3 main-col ">
+          <div class="col-md-9 left-col pull-right">
+
+              <div class="panel article-body">
+
+                  <div class="panel-body">
+
+                        <h1 class="text-center">{{ $topic->title }}</h1>
+
+                        <div class="article-meta text-center">
+                            <i class="fa fa-clock-o"></i> <abbr title="{{ $topic->created_at }}" class="timeago">{{ $topic->created_at }}</abbr>
+                            ⋅
+                            <i class="fa fa-eye"></i> {{ $topic->view_count }}
+                            ⋅
+                            <i class="fa fa-thumbs-o-up"></i> {{ $topic->vote_count }}
+                            ⋅
+                            <i class="fa fa-comments-o"></i> {{ $topic->reply_count }}
+
+                        </div>
+
+                        @include('topics.partials.body', array('body' => $topic->body))
+
+                        <div class="post-info-panel">
+                            <p class="info">
+                                <label class="info-title">版权声明：</label><i class="fa fa-fw fa-creative-commons"></i>自由转载-非商用-非衍生-保持署名（<a href="https://creativecommons.org/licenses/by-nc-nd/3.0/deed.zh">创意共享3.0许可证</a>）
+                            </p>
+                        </div>
+                        <br>
+                        <br>
+                        @include('topics.partials.topic_operate', ['is_article' => true, 'manage_topics' => $currentUser ? ($currentUser->can("manage_topics") && $currentUser->roles->count() <= 5) : false])
+                  </div>
+
+              </div>
+
+              @include('topics.partials.show_segment')
+        </div>
+
+      <div class="col-md-3 main-col pull-left">
           <div class="panel panel-default corner-radius">
 
             <div class="panel-body text-center topic-author-box">
@@ -67,41 +103,6 @@
           </div>
       </div>
 
-      <div class="col-md-9 left-col">
-
-          <div class="panel article-body">
-
-              <div class="panel-body">
-
-                    <h1 class="text-center">{{ $topic->title }}</h1>
-
-                    <div class="article-meta text-center">
-                        <i class="fa fa-clock-o"></i> <abbr title="{{ $topic->created_at }}" class="timeago">{{ $topic->created_at }}</abbr>
-                        ⋅
-                        <i class="fa fa-eye"></i> {{ $topic->view_count }}
-                        ⋅
-                        <i class="fa fa-thumbs-o-up"></i> {{ $topic->vote_count }}
-                        ⋅
-                        <i class="fa fa-comments-o"></i> {{ $topic->reply_count }}
-
-                    </div>
-
-                    @include('topics.partials.body', array('body' => $topic->body))
-
-                    <div class="post-info-panel">
-                        <p class="info">
-                            <label class="info-title">版权声明：</label><i class="fa fa-fw fa-creative-commons"></i>自由转载-非商用-非衍生-保持署名（<a href="https://creativecommons.org/licenses/by-nc-nd/3.0/deed.zh">创意共享3.0许可证</a>）
-                        </p>
-                    </div>
-                    <br>
-                    <br>
-                    @include('topics.partials.topic_operate', ['is_article' => true, 'manage_topics' => $currentUser ? ($currentUser->can("manage_topics") && $currentUser->roles->count() <= 5) : false])
-              </div>
-
-          </div>
-
-          @include('topics.partials.show_segment')
-    </div>
 </div>
 
 @stop
