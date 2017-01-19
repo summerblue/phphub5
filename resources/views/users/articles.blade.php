274c415ae4b1e3bc2f4df47a13ee09c9ad911e73
Summer <cj@estgroupe.com>
2017-01-19 10:07:37 +0800

    article coutn

--- resources/views/users/articles.blade.php
+++ resources/views/users/articles.blade.php
@@ -17,7 +17,7 @@
 
   <ol class="breadcrumb">
       <li><a href="{{ route('users.show', $user->id) }}">个人中心</a></li>
-      <li class="active">Ta 发表的文章（{{ $user->topic_count }}）</li>
+      <li class="active">Ta 发表的文章（{{ $user->article_count }}）</li>
   </ol>
 
   <div class="panel panel-default">
