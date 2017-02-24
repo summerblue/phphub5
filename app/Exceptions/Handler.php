08e40c783d661e6fc34633d06324dfca38d85368
Summer <cj@estgroupe.com>
2017-02-24 10:49:27 +0800

    500 page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use App;
 
 class Handler extends ExceptionHandler
 {
@@ -44,6 +45,10 @@ class Handler extends ExceptionHandler
     {
         if ($e instanceof ModelNotFoundException) {
             $e = new NotFoundHttpException($e->getMessage(), $e);
+        }
+
+        if ( ! config('app.debug')) {
+            return response()->view('errors.500', [], 500);
         }
 
         return parent::render($request, $e);
