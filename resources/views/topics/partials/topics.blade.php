10a840d42fdf3f72e2965659e08257c21a8cbfcc
Summer <cj@estgroupe.com>
2017-02-24 15:01:55 +0800

    seperate qa

--- resources/views/topics/partials/topics.blade.php
+++ resources/views/topics/partials/topics.blade.php
@@ -44,9 +44,9 @@
                 @if ($topic->order > 0 && !Input::get('filter') && Route::currentRouteName() != 'home' )
                     <span class="hidden-xs label label-warning">{{ lang('Stick') }}</span>
                 @else
-                    @if (!Request::is('categories/'.config('phphub.blog_category_id')))
+                    {{-- @if (!Request::is('categories/'.config('phphub.blog_category_id'))) --}}
                         <span class="hidden-xs label label-{{ ($topic->is_excellent == 'yes' && Route::currentRouteName() != 'home') ? 'success' : 'default' }}">{{{ $topic->category->name }}}</span>
-                    @endif
+                    {{-- @endif --}}
                 @endif
 
                 @if ($topic->category->id == config('phphub.blog_category_id'))
