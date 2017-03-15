373fa8a840b9834f3d467b1ec6b0c4057d44e0e1
Summer <cj@estgroupe.com>
2017-03-15 14:57:47 +0800

    acitivity view

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,115 +2,27 @@
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
-use App\Activity;
+use App\Models\Activity;
+use App\Models\Banner;
+use App\Models\Link;
+use App\Models\ActiveUser;
+use App\Models\HotTopic;
 use Illuminate\Http\Request;
+use Auth;
 
-class ActivityController extends Controller {
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
+class ActivityController extends Controller
+{
+	public function index(Request $request)
 	{
-		$activities = Activity::orderBy('id', 'desc')->paginate(10);
+        // $activities = Auth::user()->subscribedActivityFeeds();
+        $activities = Activity::paginate();
+        $links  = Link::allFromCache();
+        $banners = Banner::allByPosition();
 
-		return view('activities.index', compact('activities'));
-	}
+        $active_users = ActiveUser::fetchAll();
+        $hot_topics = HotTopic::fetchAll();
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('activities.create');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function store(Request $request)
-	{
-		$activity = new Activity();
-
-		$activity->causer = $request->input("causer");
-        $activity->type = $request->input("type");
-        $activity->indentifier = $request->input("indentifier");
-        $activity->user_id = $request->input("user_id");
-        $activity->data = $request->input("data");
-
-		$activity->save();
-
-		return redirect()->route('activities.index')->with('message', 'Item created successfully.');
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$activity = Activity::findOrFail($id);
-
-		return view('activities.show', compact('activity'));
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$activity = Activity::findOrFail($id);
-
-		return view('activities.edit', compact('activity'));
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function update(Request $request, $id)
-	{
-		$activity = Activity::findOrFail($id);
-
-		$activity->causer = $request->input("causer");
-        $activity->type = $request->input("type");
-        $activity->indentifier = $request->input("indentifier");
-        $activity->user_id = $request->input("user_id");
-        $activity->data = $request->input("data");
-
-		$activity->save();
-
-		return redirect()->route('activities.index')->with('message', 'Item updated successfully.');
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		$activity = Activity::findOrFail($id);
-		$activity->delete();
-
-		return redirect()->route('activities.index')->with('message', 'Item deleted successfully.');
+        return view('activities.index', compact('activities', 'links', 'banners', 'active_users', 'hot_topics'));
 	}
 
 }
