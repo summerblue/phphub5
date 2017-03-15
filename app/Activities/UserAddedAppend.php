1fe20d2d79dd934b307b92734f1a6334a4962576
Summer <summer@yousails.com>
2017-03-15 15:06:47 +0800

    UserAddedAppend

--- app/Activities/UserAddedAppend.php
+++ app/Activities/UserAddedAppend.php
@@ -6,5 +6,15 @@ use App\Models\Activity;
 
 class UserAddedAppend extends BaseActivity
 {
+    public function generate($user, $topic, $append)
+    {
+        $this->addActivity($user, $topic, [
+            'body' => $append->content
+        ]);
+    }
 
+    public function remove($user, $topic)
+    {
+        $this->removeBy("u$user->id", "t$topic->id");
+    }
 }
--- app/Activities/UserAttendedTopic.php
+++ app/Activities/UserAttendedTopic.php
@@ -6,5 +6,13 @@ use App\Models\Activity;
 
 class UserAttendedTopic extends BaseActivity
 {
+    public function generate($user, $topic)
+    {
+        $this->addActivity($user, $topic);
+    }
 
+    public function remove($user, $topic)
+    {
+        $this->removeBy("u$user->id", "t$topic->id");
+    }
 }
