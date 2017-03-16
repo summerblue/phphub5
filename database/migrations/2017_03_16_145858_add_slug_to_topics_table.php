1b4563a3d287fa9222c47404a134a18b43f8c7dd
Summer <cj@estgroupe.com>
2017-03-16 15:00:33 +0800

    add pinyin for translation fallback

---
+++ database/migrations/2017_03_16_145858_add_slug_to_topics_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSlugToTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('topics', function (Blueprint $table) {
+            $table->string('slug')->index()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('topics', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+    }
+}
