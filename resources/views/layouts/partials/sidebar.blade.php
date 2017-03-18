603406d8c716e3a8cd0a3e8bbff0a3a0f1a2b9ef
Summer <summer@yousails.com>
2017-03-19 04:44:38 +0800

    change title

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -52,7 +52,7 @@
   @if (isset($categoryTopics) && count($categoryTopics))
   <div class="panel panel-default corner-radius">
     <div class="panel-heading text-center">
-      <h3 class="panel-title">{{ lang('Same Category Topics') }}</h3>
+      <h3 class="panel-title">七天内最热</h3>
     </div>
     <div class="panel-body">
       @include('layouts.partials.sidebar_topics', ['sidebarTopics' => $categoryTopics])
