5c9db8925467d4cf5e15e18fa2a22f6b755afdc5
Summer <summer@yousails.com>
2017-03-23 20:07:38 +0800

    update

--- resources/views/topics/partials/show_segment.blade.php
+++ resources/views/topics/partials/show_segment.blade.php
@@ -8,9 +8,17 @@
             <a data-ajax="post" href="javascript:void(0);" data-url="{{ route('topics.upvote', $topic->id) }}" title="{{ lang('Up Vote') }}"
                 data-content="点赞相当于收藏，可以在个人页面的「赞过的话题」导航里查看"
                 id="up-vote"
-                class="vote btn btn-primary {{ $topic->user->payment_qrcode ?: 'btn-inverted' }} popover-with-html {{ $currentUser && $topic->votes()->ByWhom(Auth::id())->WithType('upvote')->count() ? 'active' :'' }}" >
+                <?php
+                    $is_voted = $currentUser && $topic->votes()->ByWhom(Auth::id())->WithType('upvote')->exists();
+                ?>
+                class="vote btn btn-primary {{ $topic->user->payment_qrcode ?: 'btn-inverted' }} popover-with-html {{  $is_voted ? 'active' :'' }}" >
+                @if ($is_voted)
+                已赞过
+                @else
                 <i class="fa fa-thumbs-up" aria-hidden="true"></i>
                 点赞
+                @endif
+
             </a>
 
             @if( $topic->user->payment_qrcode )
