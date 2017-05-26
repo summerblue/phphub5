d231b5b7cb9d11eb1608fd6ddf36bacde951b210
Summer <summer@yousails.com>
2017-05-26 11:38:33 +0800

    add hint

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -13,6 +13,11 @@
             在 LC，我们不提倡 <a href="{{ route('topics.show', 535) }}" style="text-decoration: underline;">新手提问</a> ，如果你编程遇到难题，请先 <a href="{{ route('topics.show', 3656) }}" style="text-decoration: underline;">搜索</a> 再 <a href="{{ route('topics.create', ['category_id' => config('phphub.qa_category_id')]) }}" class="btn btn-warning">提问</a>
         </div>
     @endif
+    @if (isset($category) && $category->id === 1)
+        <div class="alert alert-info">
+            发布招聘贴前必须仔细阅读 <a href="https://laravel-china.org/topics/817/laravel-china-recruitment-post-specification" style="text-decoration: underline;">Laravel China 招聘贴发布规范</a>，不按规范发帖会被管理员 <a href="https://laravel-china.org/topics/2802/description-of-shen">永久下沉</a>。<a href="{{ route('topics.create', ['category_id' => 1]) }}" class="btn btn-warning">发布招聘</a>
+        </div>
+    @endif
     <div class="panel panel-default">
 
         <div class="panel-heading">
