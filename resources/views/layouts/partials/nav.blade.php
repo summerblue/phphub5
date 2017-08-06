c1b3f2f701136c323671a95c35c5114281f794b5
Summer <summer@yousails.com>
2017-08-06 08:33:18 +0800

    update

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -23,7 +23,7 @@
         <li class="{{ (Request::is('news') ? ' active' : '') }}"><a href="https://laravel-china.org/news" class="no-pjax">资讯</a></li>
         <li class="{{ (Request::is('wiki') ? ' active' : '') }} hidden-sm"><a href="{{ route('wiki') }}">Wiki</a></li>
         <li class="nav-docs hidden-sm"><a href="http://d.laravel-china.org" class="no-pjax" target="_blank">文档</a></li>
-        <li class="hidden-sm"><a href="https://fsdhub.com/books/laravel-essential-training-5.1" class="no-pjax" target="_blank">教程</a></li>
+        <li class="hidden-sm"><a href="https://fsdhub.com/books/laravel-essential-training-5.5" class="no-pjax" target="_blank">教程</a></li>
       </ul>
 
       <div class="navbar-right">
